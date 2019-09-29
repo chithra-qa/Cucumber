c8f9b855e6d6736d39af1d27b4fca462da51b3fb
Chithra Ramachandran <chithra.ramachandran@ticgroup.com.au>
2019-09-30 09:15:20 +1000

    Adding the glue code

--- src/test/java/stepDefinitions/TestRunner.java
+++ src/test/java/stepDefinitions/TestRunner.java
@@ -1,23 +1,37 @@
 package stepDefinitions;
 
-import org.junit.runner.JUnitCore;
-import org.junit.runner.RunWith;
-import org.testng.annotations.*;
-import org.openqa.selenium.WebDriver;
+ 
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.java.After;
-import cucumber.api.junit.Cucumber;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.junit.runner.RunWith;
+
+import org.testng.annotations.*;
+
+ 
+
+import io.cucumber.junit.Cucumber;
+
+import io.cucumber.junit.CucumberOptions;
+
+ 
 
 @RunWith(Cucumber.class)
+
 @CucumberOptions(
+
         plugin ={"pretty","html:target/cucumber-html-report/","json:target/cucumber.json"},
+
         features = {"src/test/java/featureFiles"},
-        glue = {"Solvup/stepDefinitions/"},
-        tags ={"@test1"}
+
+        glue = {""},
+
+        tags ={"@test"}
+
                 )
+
 @Test
+
 public class TestRunner {
-	
+
+               
+
 }
